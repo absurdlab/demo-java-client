1baf87a05e94a132a8f698d7b87e01b9123f482a
Weinan Qiu <davidiamyou@gmail.com>
2020-12-28 07:02:12 +0800

    init project

---
+++ src/main/java/io/absurdlab/demojavaclient/DemoJavaClientApplication.java
@@ -0,0 +1,13 @@
+package io.absurdlab.demojavaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/absurdlab/demojavaclient/DemoJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.absurdlab.demojavaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
