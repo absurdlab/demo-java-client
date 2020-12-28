e6a84950aed488e8c2adc26ba3b0a106d885bc43
Weinan Qiu <davidiamyou@gmail.com>
2020-12-28 17:40:00 +0800

    demonstrates login with Tiga

---
+++ src/main/java/io/absurdlab/demojavaclient/DemoController.java
@@ -0,0 +1,20 @@
+package io.absurdlab.demojavaclient;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/oidc")
+    public OidcUser getOidcUserPrincipal(@AuthenticationPrincipal OidcUser principal) {
+        return principal;
+    }
+}
--- src/main/java/io/absurdlab/demojavaclient/DemoJavaClientApplication.java
+++ src/main/java/io/absurdlab/demojavaclient/DemoJavaClientApplication.java
@@ -2,12 +2,48 @@ package io.absurdlab.demojavaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
 
 @SpringBootApplication
 public class DemoJavaClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoJavaClientApplication.class, args);
+	@Configuration
+	public static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				.oauth2Login();
+		}
 	}
 
+	private static final TrustManager[] UNQUESTIONING_TRUST_MANAGER = new TrustManager[]{
+		new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers(){
+				return null;
+			}
+			public void checkClientTrusted( X509Certificate[] certs, String authType ){}
+			public void checkServerTrusted( X509Certificate[] certs, String authType ){}
+		}
+	};
+
+	public static void main(String[] args) throws Exception {
+		final SSLContext sc = SSLContext.getInstance("SSL");
+		sc.init( null, UNQUESTIONING_TRUST_MANAGER, null );
+		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
+		SpringApplication.run(DemoJavaClientApplication.class, args);
+	}
 }
